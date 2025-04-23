00acc883f40b95006a9cb1568590d3412f7261a4
Avinash-cloud <Avinash.work2000@gmail.com>
2025-04-23 12:31:34 +0000

    done

--- pages/api/register.js
+++ pages/api/register.js
@@ -4,7 +4,16 @@ import bcryptjs from 'bcryptjs';
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../lib/auth";
-export default async function POST(req,res) {
+
+
+
+export default async function handler(req,res) {
+
+  const method = req.method;
+
+  if (method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
   try {
     
     // Connect to MongoDB
